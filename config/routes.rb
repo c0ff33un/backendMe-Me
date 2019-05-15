8652b2c07be6bc7d934bb14308a218d237065053
Miguel Ortiz <miaortizma@unal.edu.co>
2019-05-14 20:42:25 -0500

    at least runs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :memes, shallow: true
+  
+  concern :commentable do
+  	resources :comments
   end
-  resources :posts
+  
+  concern :reactionable do
+  	resources :reactions
+  end
+  
+  resources :users, concerns: [:commentable, :reactionable] do
+  	resources :posts, :memes
+  end
+  
+  resources :memes, concerns: :reactionable do
+  	resources :post_memes
+  end
+
+  resources :posts, concerns: :commentable do
+  	resources :post_memes
+  end
+
+  resources :post_memes, concerns: :commentable
+  
+  resources :comments, :reactions
+
 end
