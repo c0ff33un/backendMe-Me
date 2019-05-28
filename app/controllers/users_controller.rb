3da3e0ebc3deab4dc098d1556bc448216ca271e9
Estudiante <Estudiante@BE401S303C16ING.unal.loc>
2019-05-28 18:42:18 -0500

    add user.valid?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
     def create
         user = User.create(user_params)
-        if user
+	if user.valid?
             render json: user, status: :created
         else
             render json: user.errors, status: :unprocessable_entity
