2189560e702b9892e452ea33ed67e491a58c1447
Miguel <miaortizma@unal.edu.co>
2019-05-29 08:19:56 -0500

    smtp mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,10 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { host: 'meemperrapi.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => "25",
+    :domain  => 'meemperrapi.herokuapp.com'
+  }
 end
