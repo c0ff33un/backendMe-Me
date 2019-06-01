86f7c75106bdb8e0785fc244fc6ea2178549aec9
Miguel <miaortizma@unal.edu.co>
2019-06-01 15:39:42 -0500

    :c

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -305,11 +305,11 @@ Devise.setup do |config|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
 
     jwt.dispatch_requests = [
-        ['POST', %r{^/api/login$}],
+        ['POST', %r{^/login$}],
         ['GET',%r{^/$}]
       ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/api/logout$}]
+      ['DELETE', %r{^/logout$}]
     ]
 
     jwt.expiration_time = 5.minutes.to_i
