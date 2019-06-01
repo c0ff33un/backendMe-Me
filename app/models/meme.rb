3f585fb7a7ef37af3818b3855cc88d37b35a6f07
Edwin Mahecha <ermahechap@unal.edu.co>
2019-06-01 18:56:03 -0500

    changed query

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -13,18 +13,17 @@ class Meme < ApplicationRecord
   has_many :reactions, dependent: :destroy
   #has_many :users, through: :reactions
 
-  def self.filter( upload_date = Time.use_zone(Time.zone.name) { 1.week.ago } ) 
-    self.where('created_at >= ?', upload_date ).pluck(:id)
+  #Queries
+  def self.filter( upload_date = Time.use_zone(Time.zone.name) { 1.week.ago }, page = 1 ) 
+    self.where(created_at:(upload_date..Time.zone.now)).pluck(:id)
+    #self.where('created_at >= ?', upload_date ).pluck(:id)
   end
 
   def self.best
-    self.left_joins(:reactions).group(:id).
-    order('COUNT(reactions.id) DESC').
-    pluck(:id)
+    self.left_joins(:reactions).group(:id).order('COUNT(reactions.id) DESC').pluck(:id)
   end
 
   def self.hot
-  
     self.left_joins(:reactions).group(:id).
     order("AVG( strftime('%Y%m%d', reactions.created_at )) DESC").
     pluck(:id)
