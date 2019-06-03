dbe73cf4e5e6e35c99bc61f28757d53db788f5f0
joacarrilloco <joacarrilloco@unal.edu.co>
2019-06-02 20:48:40 -0500

    End Points added

---
+++ app/controllers/momazos_controller.rb
@@ -0,0 +1,28 @@
+class MomazosController < ApplicationController
+    before_action :load_defaults
+
+    def best
+        render json: Meme.best.paginate(page: params[:page]).per_page(params[:per_page]), status: :ok
+    end
+
+    def hot
+        render json: Meme.hot.paginate(page: params[:page]).per_page(params[:per_page]), status: :ok
+    end
+
+    def newest
+        render json: Meme.newest.paginate(page: params[:page]).per_page(params[:per_page]), status: :ok
+    end
+
+    
+    private
+
+        def feed_params
+            params.permit(:page, :per_page)
+        end
+
+        def load_defaults
+            params[:page] = 1 if params[:page] == nil 
+            params[:per_page] = 12 if params[:per_page] == nil
+        end
+
+end
