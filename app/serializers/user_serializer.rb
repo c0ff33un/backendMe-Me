707d74d47eb49867f484bfffa74a516a5909d878
Edwin Mahecha <ermahechap@unal.edu.co>
2019-06-02 19:04:33 -0500

    minor fix in serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :handle, :birthday, :created_at, :img
+  attributes :id, :handle, :birthday, :created_at
+  attribute :img, if: :img_not_nil?
+
+  def img_not_nil?
+    true if object.picture
+  end
+
   def img
     self.object.picture.image
   end
